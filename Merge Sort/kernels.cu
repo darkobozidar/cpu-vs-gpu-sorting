3bc2546d78e6d1ae14bf0d2b7fceb047811c5a80
Darko Božidar <darko.bozidar@gmail.com>
2014-08-27 15:36:20 +0200

    Fixed calculation of indexes for generate sub-blocks kernel.

--- Merge Sort/kernels.cu
+++ Merge Sort/kernels.cu
@@ -63,15 +63,31 @@ __global__ void bitonicSortKernel(data_t* array, uint_t arrayLen, uint_t sharedM
 	}
 }
 
+__device__ uint_t calculateElementIndex(uint_t tableLen, uint_t tabBlockSize, uint_t tabSubBlockSize, bool firstHalf) {
+	// Thread index for first or second half of the table
+	uint_t threadIdxX = threadIdx.x + (!firstHalf) * blockDim.x;
+	// Index of a block which thread will read the sample from
+	uint_t indexBlock = threadIdxX / (tabBlockSize / tabSubBlockSize);
+	// Offset to block index (every thread loads 2 elements)
+	uint_t index = blockIdx.x * 2 * blockDim.x + indexBlock * tabBlockSize;
+	// Offset for sub-block index inside block for ODD block
+	index += ((indexBlock % 2 == 0) * threadIdxX * tabSubBlockSize) % tabBlockSize;
+	// Offset for sub-block index inside block for EVEN block (index has to be reversed)
+	index += ((indexBlock % 2 == 1) * (tabBlockSize - (threadIdxX + 1) * tabSubBlockSize)) % tabBlockSize;
+
+	return index;
+}
+
 __global__ void generateSublocksKernel(data_t* table, uint_t tableLen, uint_t tabBlockSize, uint_t tabSubBlockSize) {
 	extern __shared__ data_t tile[];
-	int index = blockIdx.x * 2 * blockDim.x + threadIdx.x * tabSubBlockSize;
+	uint_t index1 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, true);
+	uint_t index2 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, false);
 
-	if (index < tableLen) {
-		tile[threadIdx.x] = table[index];
+	if (index1 < tableLen) {
+		tile[threadIdx.x] = table[index1];
 	}
-	if (index + tableLen / 2 < tableLen) {
-		tile[threadIdx.x + blockDim.x] = table[index + tableLen / 2];
+	if (index2 + tableLen / 2 < tableLen) {
+		tile[threadIdx.x + blockDim.x] = table[index2];
 	}
 
 	for (uint_t stride = blockDim.x / 2; stride > 0; stride /= 2) {
