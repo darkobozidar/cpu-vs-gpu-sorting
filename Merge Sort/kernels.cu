23f3d27b901d22e5ec4ee816fa83176a6e2e6575
Darko Božidar <darko.bozidar@gmail.com>
2014-08-27 19:01:38 +0200

    Fixed reading of samples in coalesced way. Fixed thread indexing to shared memory indexes (previously global memory indexes).

--- Merge Sort/kernels.cu
+++ Merge Sort/kernels.cu
@@ -64,37 +64,52 @@ __global__ void bitonicSortKernel(data_t* array, uint_t arrayLen, uint_t sharedM
 }
 
 __device__ uint_t calculateElementIndex(uint_t tableLen, uint_t tabBlockSize, uint_t tabSubBlockSize, bool firstHalf) {
-	// Thread index for first or second half of the table
+	// Thread index for first or second half of the sub-table
 	uint_t threadIdxX = threadIdx.x + (!firstHalf) * blockDim.x;
-	// Index of a block which thread will read the sample from
-	uint_t indexBlock = threadIdxX / (tabBlockSize / tabSubBlockSize);
-	// Offset to block index (every thread loads 2 elements)
-	uint_t index = blockIdx.x * 2 * blockDim.x + indexBlock * tabBlockSize;
+	uint_t subBlocksPerBlock = tabBlockSize / tabSubBlockSize;
+	// Index of a block from which thread will read the sample
+	uint_t indexBlock = threadIdxX / subBlocksPerBlock;
+	// Offset to block index (we devide and multiply with same value, to lose the offset to sub-block)
+	uint_t index = indexBlock * subBlocksPerBlock;
 	// Offset for sub-block index inside block for ODD block
-	index += ((indexBlock % 2 == 0) * threadIdxX * tabSubBlockSize) % tabBlockSize;
+	index += ((indexBlock % 2 == 0) * threadIdxX) % subBlocksPerBlock;
 	// Offset for sub-block index inside block for EVEN block (index has to be reversed)
-	index += ((indexBlock % 2 == 1) * (tabBlockSize - (threadIdxX + 1) * tabSubBlockSize)) % tabBlockSize;
+	index += ((indexBlock % 2 == 1) * (subBlocksPerBlock - (threadIdxX + 1))) % subBlocksPerBlock;
 
 	return index;
 }
 
+__device__ void printfOnce(char* text) {
+	if (threadIdx.x == 0) {
+		printf(text);
+	}
+}
+
 __global__ void generateSublocksKernel(data_t* table, uint_t tableLen, uint_t tabBlockSize, uint_t tabSubBlockSize) {
 	extern __shared__ data_t tile[];
-	uint_t index1 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, true);
-	uint_t index2 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, false);
+	uint_t index = blockIdx.x * 2 * blockDim.x + threadIdx.x * tabSubBlockSize;
+	uint_t sharedMemIdx1, sharedMemIdx2;
+	data_t value1, value2;
 
-	if (index1 < tableLen) {
-		tile[threadIdx.x] = table[index1];
+	// Values are read in coalesced way...
+	if (index < tableLen) {
+		value1 = table[index];
 	}
-	if (index2 < tableLen) {
-		tile[threadIdx.x + blockDim.x] = table[index2];
+	if (index + blockDim.x < tableLen) {
+		value2 = table[index + blockDim.x * tabSubBlockSize];
 	}
+
+	// ...and than reversed when added to shared memory
+	sharedMemIdx1 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, true);
+	sharedMemIdx2 = calculateElementIndex(tableLen, tabBlockSize, tabSubBlockSize, false);
+	tile[sharedMemIdx1] = value1;
+	tile[sharedMemIdx2] = value2;
 
 	for (uint_t stride = tabBlockSize / tabSubBlockSize; stride > 0; stride /= 2) {
 		__syncthreads();
 		uint_t index = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
 
-		// TODO use max/min or conditional operator
+		// TODO use max/min or conditional operator (or something else)
 		if (tile[index] > tile[index + stride]) {
 			data_t temp = tile[index];
 			tile[index] = tile[index + stride];
