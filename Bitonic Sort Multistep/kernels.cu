4544edccc89e474af98394caf04908426616d9dc
Darko Božidar <darko.bozidar@gmail.com>
2014-09-15 14:26:16 +0200

    Small refactor of bitonic sort multistep kernels.

--- Bitonic Sort Multistep/kernels.cu
+++ Bitonic Sort Multistep/kernels.cu
@@ -31,6 +31,8 @@ Sorts sub-blocks of input data with bitonic sort.
 */
 __global__ void bitonicSortKernel(el_t *table, bool orderAsc) {
     extern __shared__ el_t sortTile[];
+    // If shared memory size is lower than table length, than every block has to be ordered
+    // in opposite direction -> bitonic sequence.
     bool blockDirection = orderAsc ^ (blockIdx.x & 1);
 
     // Every thread loads 2 elements
@@ -38,12 +40,12 @@ __global__ void bitonicSortKernel(el_t *table, bool orderAsc) {
     sortTile[threadIdx.x] = table[index];
     sortTile[blockDim.x + threadIdx.x] = table[blockDim.x + index];
 
+    // Bitonic sort
     for (uint_t subBlockSize = 1; subBlockSize <= blockDim.x; subBlockSize <<= 1) {
         bool direction = blockDirection ^ ((threadIdx.x & subBlockSize) != 0);
 
         for (uint_t stride = subBlockSize; stride > 0; stride >>= 1) {
             __syncthreads();
-            // In first step of every phase END index has to be reversed
             uint_t start = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
             compareExchange(&sortTile[start], &sortTile[start + stride], direction);
         }
@@ -105,23 +107,21 @@ __global__ void multiStepKernel(el_t *table, uint_t phase, uint_t step, uint_t d
 }
 
 /*
-Sorts sub-blocks of input data with bitonic sort.
+Global bitonic merge for sections, where stride IS LOWER OR EQUAL than max shared memory.
 */
 __global__ void bitonicMergeKernel(el_t *table, uint_t phase, bool orderAsc) {
     extern __shared__ el_t mergeTile[];
-    uint_t threadIndex = blockIdx.x * blockDim.x + threadIdx.x;
-    uint_t threadsPerSubBlock = 1 << (phase - 1);
-    bool subBlockDirection = (threadIndex / threadsPerSubBlock) % 2;
+    uint_t index = blockIdx.x * 2 * blockDim.x + threadIdx.x;
+    // Elements inside same sub-block have to be ordered in same direction
+    bool direction = orderAsc ^ ((index >> phase) & 1);
 
     // Every thread loads 2 elements
-    uint_t index = blockIdx.x * 2 * blockDim.x + threadIdx.x;
     mergeTile[threadIdx.x] = table[index];
     mergeTile[blockDim.x + threadIdx.x] = table[blockDim.x + index];
 
-    uint_t direction = orderAsc ^ ((threadIdx.x & blockDim.x) != 0) ^ subBlockDirection;
+    // Bitonic merge
     for (uint_t stride = blockDim.x; stride > 0; stride >>= 1) {
         __syncthreads();
-        // In first step of every phase END index has to be reversed
         uint_t start = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
         compareExchange(&mergeTile[start], &mergeTile[start + stride], direction);
     }
