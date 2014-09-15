070482926da359d4134eb682e92d873728c4ac42
Darko Božidar <darko.bozidar@gmail.com>
2014-09-15 13:57:26 +0200

    Simplified bitonic sort kernels.

--- Bitonic Sort/kernels.cu
+++ Bitonic Sort/kernels.cu
@@ -33,7 +33,7 @@ __global__ void bitonicSortKernel(el_t *table, bool orderAsc) {
     sortTile[threadIdx.x] = table[index];
     sortTile[blockDim.x + threadIdx.x] = table[blockDim.x + index];
 
-    // Bitonic sort of shared memory
+    // Bitonic sort
     for (uint_t subBlockSize = 1; subBlockSize <= blockDim.x; subBlockSize <<= 1) {
         bool direction = blockDirection ^ ((threadIdx.x & subBlockSize) != 0);
 
@@ -57,7 +57,7 @@ __global__ void bitonicMergeGlobalKernel(el_t *table, uint_t phase, uint_t step,
     uint_t indexThread = blockIdx.x * blockDim.x + threadIdx.x;
     uint_t indexTable = 2 * indexThread - (indexThread & (stride - 1));
     // Elements inside same sub-block have to be ordered in same direction
-    bool direction = orderAsc ^ ((indexThread >> (phase - 1)) & 1);
+    bool direction = orderAsc ^ ((indexTable >> phase) & 1);
 
     el_t el1 = table[indexTable];
     el_t el2 = table[indexTable + stride];
@@ -73,20 +73,17 @@ Global bitonic merge for sections, where stride IS LOWER OR EQUAL than max share
 */
 __global__ void bitonicMergeLocalKernel(el_t *table, uint_t phase, bool orderAsc) {
     extern __shared__ el_t mergeTile[];
-    uint_t threadIndex = blockIdx.x * blockDim.x + threadIdx.x;
-    uint_t threadsPerSubBlock = 1 << (phase - 1);
+    uint_t index = blockIdx.x * 2 * blockDim.x + threadIdx.x;
     // Elements inside same sub-block have to be ordered in same direction
-    bool subBlockDirection = (threadIndex / threadsPerSubBlock) % 2;
-    uint_t direction = orderAsc ^ ((threadIdx.x & blockDim.x) != 0) ^ subBlockDirection;
+    bool direction = orderAsc ^ ((index >> phase) & 1);
 
     // Every thread loads 2 elements
-    uint_t index = blockIdx.x * 2 * blockDim.x + threadIdx.x;
     mergeTile[threadIdx.x] = table[index];
     mergeTile[blockDim.x + threadIdx.x] = table[blockDim.x + index];
 
+    // Bitonic merge
     for (uint_t stride = blockDim.x; stride > 0; stride >>= 1) {
         __syncthreads();
-        // In first step of every phase END index has to be reversed
         uint_t start = 2 * threadIdx.x - (threadIdx.x & (stride - 1));
         compareExchange(&mergeTile[start], &mergeTile[start + stride], direction);
     }
