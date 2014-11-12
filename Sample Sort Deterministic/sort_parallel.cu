80135a7a714f3370e5d67e5b5d47cdc11d1b84b6
Darko Božidar <darko.bozidar@gmail.com>
2014-11-12 12:53:41 +0100

    Added calculation of samples length, allocated memory for samples.

--- Sample Sort Deterministic/sort_parallel.cu
+++ Sample Sort Deterministic/sort_parallel.cu
@@ -15,13 +15,16 @@
 /*
 Initializes DEVICE memory needed for paralel sort implementation.
 */
-void memoryInit(el_t *h_input, el_t **d_dataInput, el_t **d_dataBuffer, uint_t tableLen) {
+void memoryInit(el_t *h_input, el_t **d_dataInput, el_t **d_dataBuffer, data_t **d_samples, uint_t tableLen,
+                uint_t localSamplesLen) {
     cudaError_t error;
 
     // Data memory allocation
     error = cudaMalloc(d_dataInput, tableLen * sizeof(**d_dataInput));
     checkCudaError(error);
     error = cudaMalloc(d_dataBuffer, tableLen * sizeof(**d_dataBuffer));
+    checkCudaError(error);
+    error = cudaMalloc(d_samples, localSamplesLen * sizeof(**d_samples));
     checkCudaError(error);
 
     error = cudaMemcpy(*d_dataInput, h_input, tableLen * sizeof(**d_dataInput), cudaMemcpyHostToDevice);
@@ -42,7 +45,7 @@ void runBitoicSortKernel(el_t *dataTable, uint_t tableLen, order_t sortOrder) {
     startStopwatch(&timer);
     bitonicSortKernel << <dimGrid, dimBlock, elemsPerThreadBlock * sizeof(*dataTable) >> >(
         dataTable, tableLen, sortOrder
-        );
+    );
     /*error = cudaDeviceSynchronize();
     checkCudaError(error);
     endStopwatch(timer, "Executing bitonic sort kernel");*/
@@ -54,28 +57,44 @@ void runPrintTableKernel(el_t *table, uint_t tableLen) {
     checkCudaError(error);
 }
 
-void sampleSort(el_t *dataTable, el_t *dataBuffer, uint_t tableLen, order_t sortOrder) {
+el_t* sampleSort(el_t *dataTable, el_t *dataBuffer, uint_t tableLen, uint_t localSamplesLen, order_t sortOrder) {
     runBitoicSortKernel(dataTable, tableLen, sortOrder);
+
+    uint_t initBitonicSortSize = THREADS_PER_BITONIC_SORT * ELEMS_PER_THREAD_BITONIC_SORT;
+    if (initBitonicSortSize <= tableLen) {
+        return dataTable;
+    }
+
+    // TODO other steps
+    // TODO handle case, if all samples are the same
+    return dataTable;
 }
 
 void sortParallel(el_t *h_dataInput, el_t *h_dataOutput, uint_t tableLen, order_t sortOrder) {
-    el_t *d_dataInput, *d_dataBuffer;
+    el_t *d_dataInput, *d_dataBuffer, *d_dataResult;
+    data_t *d_samples;
+
+    uint_t initBitonicSortSize = THREADS_PER_BITONIC_SORT * ELEMS_PER_THREAD_BITONIC_SORT;
+    uint_t localSamplesLen = tableLen / initBitonicSortSize * NUM_SAMPLES;
+    localSamplesLen += (tableLen % initBitonicSortSize) / NUM_SAMPLES;
 
     LARGE_INTEGER timer;
     cudaError_t error;
 
-    memoryInit(h_dataInput, &d_dataInput, &d_dataBuffer, tableLen);
+    memoryInit(h_dataInput, &d_dataInput, &d_dataBuffer, &d_samples, tableLen, localSamplesLen);
 
     startStopwatch(&timer);
-    sampleSort(d_dataInput, d_dataBuffer, tableLen, sortOrder);
+    d_dataResult = sampleSort(d_dataInput, d_dataBuffer, tableLen, localSamplesLen, sortOrder);
 
     error = cudaDeviceSynchronize();
     checkCudaError(error);
     double time = endStopwatch(timer, "Executing parallel sample sort.");
     printf("Operations (pair swaps): %.2f M/s\n", tableLen / 500.0 / time);
 
-    error = cudaMemcpy(h_dataOutput, d_dataInput, tableLen * sizeof(*h_dataOutput), cudaMemcpyDeviceToHost);
+    error = cudaMemcpy(h_dataOutput, d_dataResult, tableLen * sizeof(*h_dataOutput), cudaMemcpyDeviceToHost);
     checkCudaError(error);
 
     cudaFree(d_dataInput);
+    cudaFree(d_dataBuffer);
+    cudaFree(d_samples);
 }
