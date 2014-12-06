69f88ba6344bf4faeaa04908028b6aaabd5bca6e
Darko Božidar <darko.bozidar@gmail.com>
2014-12-06 17:45:25 +0100

    Optimized parameters for bitonic multistep sort with key-value pairs.

--- Bitonic Sort Multistep (Key-Value)/constants.h
+++ Bitonic Sort Multistep (Key-Value)/constants.h
@@ -5,7 +5,7 @@
 
 // How many threads are used per one thread block for bitonic sort, which is performed entirely
 // in shared memory. Has to be power of 2.
-#define THREADS_PER_BITONIC_SORT 256
+#define THREADS_PER_BITONIC_SORT 128
 // How many elements are processed by one thread in bitonic sort kernel. Min value is 2.
 // Has to be divisable by 2.
 #define ELEMS_PER_THREAD_BITONIC_SORT 4
@@ -32,7 +32,7 @@
 /* --------------- BITONIC MERGE LOCAL --------------- */
 
 // How many threads are used per one thread block in LOCAL bitonic merge. Has to be power of 2.
-#define THREADS_PER_LOCAL_MERGE 256
+#define THREADS_PER_LOCAL_MERGE 128
 // How many elements are processed by one thread in LOCAL bitonic merge. Min value is 2.
 // Has to be divisable by 2.
 #define ELEMS_PER_THREAD_LOCAL_MERGE 4
