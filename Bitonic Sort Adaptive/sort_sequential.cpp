bcf0a39e590a20b635866bb8df9b426dfc2bfba0
Darko Božidar <darko.bozidar@gmail.com>
2014-12-17 16:54:35 +0100

    Added implementation of bitonic merge.

--- Bitonic Sort Adaptive/sort_sequential.cpp
+++ Bitonic Sort Adaptive/sort_sequential.cpp
@@ -24,11 +24,12 @@ void constructBitonicTree(node_t *parent, uint_t stride)
     constructBitonicTree(parent->right, stride / 2);
 }
 
-node_t* constructBitonicTree(data_t *dataTable, uint_t tableLen)
+void constructBitonicTree(data_t *dataTable, uint_t tableLen, node_t **root, node_t **spare)
 {
-    node_t *root = new node_t(dataTable + tableLen / 2 - 1);
-    constructBitonicTree(root, tableLen / 4);
-    return root;
+    *root = new node_t(dataTable + tableLen / 2 - 1);
+    *spare = new node_t(dataTable + tableLen - 1);
+
+    constructBitonicTree(*root, tableLen / 4);
 }
 
 
@@ -56,14 +57,93 @@ void printBitonicTree(node_t *node)
     printBitonicTree(node, 0);
 }
 
+void swapNodeValues(node_t *node1, node_t *node2)
+{
+    data_t value = *node1->value;
+    *node1->value = *node2->value;
+    *node2->value = value;
+}
+
+void swapLeftNode(node_t *node1, node_t *node2)
+{
+    node_t *temp = node1->left;
+    node1->left = node2->left;
+    node2->left = temp;
+}
+
+void swapRightNode(node_t *node1, node_t *node2)
+{
+    node_t *temp = node1->right;
+    node1->right = node2->right;
+    node2->right = temp;
+}
+
+void bitonicMerge(node_t *root, node_t *spare, order_t sortOrder)
+{
+    bool rightExchange = (*root->value > *spare->value) ^ sortOrder;
+
+    if (rightExchange)
+    {
+        swapNodeValues(root, spare);
+    }
+
+    node_t *leftNode = root->left;
+    node_t *rightNode = root->right;
+
+    while (leftNode != NULL)
+    {
+        bool elementExchange = (*leftNode->value > *rightNode->value) ^ sortOrder;
+
+        if (rightExchange)
+        {
+            if (elementExchange)
+            {
+                swapNodeValues(leftNode, rightNode);
+                swapRightNode(leftNode, rightNode);
+
+                leftNode = leftNode->left;
+                rightNode = rightNode->left;
+            }
+            else
+            {
+                leftNode = leftNode->right;
+                rightNode = rightNode->right;
+            }
+        }
+        else
+        {
+            if (elementExchange)
+            {
+                swapNodeValues(leftNode, rightNode);
+                swapLeftNode(leftNode, rightNode);
+
+                leftNode = leftNode->right;
+                rightNode = rightNode->right;
+            }
+            else
+            {
+                leftNode = leftNode->left;
+                rightNode = rightNode->left;
+            }
+        }
+    }
+
+    if (root->left != NULL)
+    {
+        bitonicMerge(root->left, root, sortOrder);
+        bitonicMerge(root->right, spare, sortOrder);
+    }
+}
 
 /*
 Sorts data sequentially with NORMALIZED bitonic sort.
 */
 double sortSequential(data_t* dataTable, uint_t tableLen, order_t sortOrder)
 {
-    node_t *root = constructBitonicTree(dataTable, tableLen);
-    printBitonicTree(root);
+    node_t *root, *spare;
+
+    constructBitonicTree(dataTable, tableLen, &root, &spare);
+    bitonicMerge(root, spare, sortOrder);
 
     return 10;
 }
