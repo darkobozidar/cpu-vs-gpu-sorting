76cb30e9090a14299c0f646a8e0f027bece5def0
Darko Božidar <darko.bozidar@gmail.com>
2014-12-25 15:28:18 +0100

    Added comments to sequential merge sort.

--- Merge Sort/sort_sequential.cpp
+++ Merge Sort/sort_sequential.cpp
@@ -7,6 +7,9 @@
 #include "../Utils/host.h"
 
 
+/*
+From provided table offset, size of table block and length of entire table returns end index of the block.
+*/
 uint_t getEndIndex(uint_t offset, uint_t subBlockSize, uint_t tableLen)
 {
     uint_t endIndex = offset + subBlockSize;
@@ -16,6 +19,10 @@ uint_t getEndIndex(uint_t offset, uint_t subBlockSize, uint_t tableLen)
 
 /*
 Sorts data sequentially with merge sort.
+
+Pointers to data table and data buffer are carried by reference in order to insure that output data in always
+in primary array. If it wasn't for this, there would be 50% chance (depending on array size) that output would
+be in buffer array.
 */
 double sortSequential(data_t *&dataTable, data_t *&dataBuffer, uint_t tableLen, order_t sortOrder)
 {
@@ -24,27 +31,34 @@ double sortSequential(data_t *&dataTable, data_t *&dataBuffer, uint_t tableLen, 
 
     uint_t tableLenPower2 = nextPowerOf2(tableLen);
 
+    // Log(tableLen) phases of merge sort
     for (uint_t sortedBlockSize = 2; sortedBlockSize <= tableLenPower2; sortedBlockSize *= 2)
     {
+        // Number of merged blocks that will be created in this iteration
         uint_t numBlocks = (tableLen - 1) / sortedBlockSize + 1;
+        // Number of sub-blocks being merged
         uint_t subBlockSize = sortedBlockSize / 2;
 
+        // Merge of all blocks
         for (uint_t blockIndex = 0; blockIndex < numBlocks; blockIndex++)
         {
+            // Odd (left) block being merged
             uint_t oddIndex = blockIndex * sortedBlockSize;
             uint_t oddEnd = getEndIndex(oddIndex, subBlockSize, tableLen);
 
+            // If there is only odd block without even block, then only odd block is coppied into buffer
             if (oddEnd == tableLen)
             {
                 std::copy(dataTable + oddIndex, dataTable + oddEnd, dataBuffer + oddIndex);
                 continue;
             }
 
+            // Even (right) block being merged
             uint_t evenIndex = oddIndex + subBlockSize;
             uint_t evenEnd = getEndIndex(evenIndex, subBlockSize, tableLen);
             uint_t mergeIndex = oddIndex;
 
-            // Merge
+            // Merge of odd and even block
             while (oddIndex < oddEnd && evenIndex < evenEnd)
             {
                 data_t oddElement = dataTable[oddIndex];
@@ -62,6 +76,7 @@ double sortSequential(data_t *&dataTable, data_t *&dataBuffer, uint_t tableLen, 
                 }
             }
 
+            // Block that wasn't merged entirely is coppied into buffer array
             if (oddIndex == oddEnd)
             {
                 std::copy(dataTable + evenIndex, dataTable + evenEnd, dataBuffer + mergeIndex);
