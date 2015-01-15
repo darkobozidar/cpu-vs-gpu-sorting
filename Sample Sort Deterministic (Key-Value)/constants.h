21c82a63423f4dd3dc03902e937b6b59b2cc3bbe
Darko Božidar <darko.bozidar@gmail.com>
2015-01-15 14:44:46 +0100

    Optimized parameters for parallel sample sort.

--- Sample Sort Deterministic (Key-Value)/constants.h
+++ Sample Sort Deterministic (Key-Value)/constants.h
@@ -20,7 +20,7 @@
 
 // How many threads are used per one thread block for bitonic sort, which is performed entirely
 // in shared memory. Has to be power of 2.
-#define THREADS_PER_BITONIC_SORT 512
+#define THREADS_PER_BITONIC_SORT 256
 // How many elements are processed by one thread in bitonic sort kernel. Min value is 2.
 // Has to be divisable by 2.
 #define ELEMS_PER_THREAD_BITONIC_SORT 4
@@ -29,7 +29,7 @@
 /* --------------- BITONIC MERGE GLOBAL -------------- */
 
 // How many threads are used per one thread block in GLOBAL bitonic merge. Has to be power of 2.
-#define THREADS_PER_GLOBAL_MERGE 256
+#define THREADS_PER_GLOBAL_MERGE 128
 // How many elements are processed by one thread in GLOBAL bitonic merge. Min value is 2.
 // Has to be divisable by 2.
 #define ELEMS_PER_THREAD_GLOBAL_MERGE 4
@@ -38,7 +38,7 @@
 /* --------------- BITONIC MERGE LOCAL --------------- */
 
 // How many threads are used per one thread block in LOCAL bitonic merge. Has to be power of 2.
-#define THREADS_PER_LOCAL_MERGE 512
+#define THREADS_PER_LOCAL_MERGE 256
 // How many elements are processed by one thread in LOCAL bitonic merge. Min value is 2.
 // Has to be divisable by 2.
 #define ELEMS_PER_THREAD_LOCAL_MERGE 4
@@ -54,7 +54,7 @@
 
 // How many threads are used per one thread block in kernel for buckets relocation. Has to be power of 2.
 // Also has to be greater or equal than NUM_SAMPLES. Has to be multiple of NUM_SAMPLES.
-#define THREADS_PER_BUCKETS_RELOCATION 256
+#define THREADS_PER_BUCKETS_RELOCATION 128
 
 
 /* --------- SEQUENTIAL ALGORITHM PARAMETERS --------- */
