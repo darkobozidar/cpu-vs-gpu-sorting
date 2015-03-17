6a041fcaf8c42cc5b6220072b1d54be4955bab73
Darko Božidar <darko.bozidar@gmail.com>
2015-03-17 18:45:40 +0100

    Added memory allocation for parallel merge sort.

--- MergeSort/sort_parallel.h
+++ MergeSort/sort_parallel.h
@@ -41,37 +41,88 @@ class MergeSortParallelBase : public SortParallel, public AddPaddingBase<threads
 protected:
     std::string _sortName = "Merge sort parallel";
 
-    ///*
-    //Wrapper for bitonic sort method.
-    //The code runs faster if arguments are passed to method. If members are accessed directly, code runs slower.
-    //*/
-    //void sortKeyOnly()
-    //{
-    //    if (_sortOrder == ORDER_ASC)
-    //    {
-    //        bitonicSortParallel<ORDER_ASC, true>(_d_keys, NULL, _arrayLength);
-    //    }
-    //    else
-    //    {
-    //        bitonicSortParallel<ORDER_DESC, true>(_d_keys, NULL, _arrayLength);
-    //    }
-    //}
+    // Device buffer for keys and values
+    data_t *_d_keysBuffer = NULL, *_d_valuesBuffer = NULL;
+    // Holds ranks of all even and odd subblocks, that have to be merged
+    uint_t *_d_ranksEven = NULL, *_d_ranksOdd = NULL;
 
-    ///*
-    //wrapper for bitonic sort method.
-    //the code runs faster if arguments are passed to method. if members are accessed directly, code runs slower.
-    //*/
-    //void sortKeyValue()
-    //{
-    //    if (_sortOrder == ORDER_ASC)
-    //    {
-    //        bitonicSortParallel<ORDER_ASC, false>(_d_keys, _d_values, _arrayLength);
-    //    }
-    //    else
-    //    {
-    //        bitonicSortParallel<ORDER_DESC, false>(_d_keys, _d_values, _arrayLength);
-    //    }
-    //}
+    /*
+    Method for allocating memory needed both for key only and key-value sort.
+    */
+    virtual void memoryAllocate(data_t *h_keys, data_t *h_values, uint_t arrayLength)
+    {
+        uint_t arrayLenPower2 = nextPowerOf2(arrayLength);
+        uint_t ranksLength = (arrayLenPower2 - 1) / min(subBlockSizeKo, subBlockSizeKv) + 1;
+        cudaError_t error;
+
+        SortParallel::memoryAllocate(h_keys, h_values, arrayLenPower2);
+
+        error = cudaMalloc((void **)&_d_keysBuffer, arrayLenPower2 * sizeof(*_d_keysBuffer));
+        checkCudaError(error);
+        error = cudaMalloc((void **)&_d_valuesBuffer, arrayLenPower2 * sizeof(*_d_valuesBuffer));
+        checkCudaError(error);
+
+        error = cudaMalloc((void **)&_d_ranksEven, ranksLength * sizeof(*_d_ranksEven));
+        checkCudaError(error);
+        error = cudaMalloc((void **)&_d_ranksOdd, ranksLength * sizeof(*_d_ranksOdd));
+        checkCudaError(error);
+    }
+
+    /*
+    Method for destroying memory needed for sort. For sort testing purposes this method is public.
+    */
+    void memoryDestroy()
+    {
+        if (_arrayLength == 0)
+        {
+            return;
+        }
+
+        SortParallel::memoryDestroy();
+        cudaError_t error;
+
+        error = cudaFree(_d_keysBuffer);
+        checkCudaError(error);
+        error = cudaFree(_d_valuesBuffer);
+        checkCudaError(error);
+
+        error = cudaFree(_d_ranksEven);
+        checkCudaError(error);
+        error = cudaFree(_d_ranksOdd);
+        checkCudaError(error);
+    }
+
+    /*
+    Wrapper for bitonic sort method.
+    The code runs faster if arguments are passed to method. If members are accessed directly, code runs slower.
+    */
+    void sortKeyOnly()
+    {
+        //if (_sortOrder == ORDER_ASC)
+        //{
+        //    bitonicSortParallel<ORDER_ASC, true>(_d_keys, NULL, _arrayLength);
+        //}
+        //else
+        //{
+        //    bitonicSortParallel<ORDER_DESC, true>(_d_keys, NULL, _arrayLength);
+        //}
+    }
+
+    /*
+    wrapper for bitonic sort method.
+    the code runs faster if arguments are passed to method. if members are accessed directly, code runs slower.
+    */
+    void sortKeyValue()
+    {
+        //if (_sortOrder == ORDER_ASC)
+        //{
+        //    bitonicSortParallel<ORDER_ASC, false>(_d_keys, _d_values, _arrayLength);
+        //}
+        //else
+        //{
+        //    bitonicSortParallel<ORDER_DESC, false>(_d_keys, _d_values, _arrayLength);
+        //}
+    }
 
 public:
     std::string getSortName()
