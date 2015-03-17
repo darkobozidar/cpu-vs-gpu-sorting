42a5ef316453e00ae7f8de8d211c29c68d6ed518
Darko Božidar <darko.bozidar@gmail.com>
2015-03-17 14:22:04 +0100

    Removed old projects for adaptive bitonic sort.

--- Bitonic Sort Adaptive (Key-Value)/sort_sequential.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORT_SEQUENTIAL_H
-#define SORT_SEQUENTIAL_H
-
-#include "../Utils/data_types_common.h"
-
-
-double sortSequential(data_t *keys, data_t *values, uint_t tableLen, order_t sortOrder);
-
-#endif
