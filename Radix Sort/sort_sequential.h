4601ae91a8fb8f26f0812ffc91cd5a9f8ea9bdae
Darko Božidar <darko.bozidar@gmail.com>
2015-03-19 23:55:20 +0100

    Removed old radix sort algorithms.

--- Radix Sort/sort_sequential.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORT_SEQUENTIAL_H
-#define SORT_SEQUENTIAL_H
-
-
-double sortSequential(
-    data_t *&dataInput, data_t *&dataOutput, uint_t *dataCounters, uint_t tableLen, order_t sortOrder
-);
-
-#endif
